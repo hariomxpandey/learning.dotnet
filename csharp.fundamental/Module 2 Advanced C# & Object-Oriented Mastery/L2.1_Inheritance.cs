7298d97ee0bc18766d9012f309ca34036d06cc9a
Hariom Pandey <hariomxpandey@gmail.com>
2026-09-24 15:02:15 +0530

    Add C# collections & error handling lessons to Module 1
    Added lessons and code files for List<T>, Dictionary, ArrayList, and exception handling (including custom exceptions) in Module 1. Updated progress checklists for new content. Added Module 2 inheritance lesson placeholder with basic structure.

---
+++ csharp.fundamental/Module 2 Advanced C# & Object-Oriented Mastery/L2.1_Inheritance.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace csharp.fundamental.Module_2_Advanced_C____Object_Oriented_Mastery
+{
+    internal class L2
+    {
+    }
+}
